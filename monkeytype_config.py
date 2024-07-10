b851f0af00a0c4586e49aa2a3f22c4eeaf393099
beraltan <112805885+beraltan@users.noreply.github.com>
2024-07-10 19:21:38 +0200

    skqjqj

--- monkeytype_config.py
+++ monkeytype_config.py
@@ -1,11 +1,11 @@
 from monkeytype.config import DefaultConfig
 
 class MyConfig(DefaultConfig):
+    def trace_module(self, module_name: str) -> bool:
+        # Trace only modules in the src package
+        return module_name.startswith('src.')
+
     def python_paths(self):
         return ['src']
-
-    def tracer(self):
-        from monkeytype.tracing import CallTracer
-        return CallTracer()
 
 CONFIG = MyConfig()
