6c26d297b2cbfee28a5335d72f97d01dc09105be
beraltan <112805885+beraltan@users.noreply.github.com>
2024-07-10 19:11:19 +0200

    Update add_type_hints.py

--- add_type_hints.py
+++ add_type_hints.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Step 1: Run your tests with MonkeyType tracing
-subprocess.run(["monkeytype", "run", "your_test_script.py"])
+subprocess.run(["monkeytype", "run", "test_example_module.py"])
 
 # Step 2: Apply the generated type hints
 subprocess.run(["monkeytype", "apply", "your_module_name"])
